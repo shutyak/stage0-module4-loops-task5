1dc136a024872f9ec57cd89b4d57c1e3e9b8703f
vlshutyak <vl.shutyak@gmail.com>
2023-04-17 18:53:22 +0300

    s0-m4-t5 Cross

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i==(sideLength)/2){
+                    System.out.print("8");
+                } else if (j==(sideLength)/2) {
+                    System.out.print("8");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
 
     }
 }
