11b6038d4f81b3f395f6365a773e1cc66a5406de
vlshutyak <vl.shutyak@gmail.com>
2023-04-17 18:38:56 +0300

    s0-m4-t5 Square

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
+    public void printSquareFrom8s(int sideLength) {
+        // Write a program that will write to console an empty square of a given size(it should consist of '8'-s).
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i == 0 || i == sideLength - 1 ||j==0||j==sideLength-1) {
+                    System.out.print("8");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
 
     }
 }
